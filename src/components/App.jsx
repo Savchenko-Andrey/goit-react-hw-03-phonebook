416ec3884f2d8c7b6cae40a8ac8d52852ffe4544
Savchenko-Andrey <dracxm95@gmail.com>
2022-10-07 12:37:16 +0300

    change 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis soluta ut reiciendis nesciunt aliquid ullam, autem, modi vero nemo tempore facilis culpa laboriosam expedita dolores optio laudantium. Quaerat, non quibusdam!
     </div>
   );
 };
